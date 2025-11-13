f0f020c1777b5660fe2b1fb3544c7dde32b7cbe4
mnit <mnit@mnit-OptiPlex-5040>
2025-11-13 15:09:44 +0530

    Adding new python file

---
+++ vecadd.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+v1 = np.array(list(map(float, input("Enter first vector (x1 y1): ").split())))
+v2 = np.array(list(map(float, input("Enter second vector (x2 y2): ").split())))
+
+dot_product = np.dot(v1, v2)
+
+print(f"The dot product of the two vectors is: {dot_product}")
+print("This is my commit for vecadd.py file.")
+print("solved the bug - commit")
+print("This is my commit for vecadd.py file in parallel branch")
+print("using stash to ... temporariy store incomplete changes without commiting and then getting changes back by stash pop")
